fc1cf671d30cf055128d0d43db5c1573d1958795
suleohis <86976610+suleohis@users.noreply.github.com>
2022-08-04 16:19:49 +0100

    First Commit

---
+++ curlTest.c
@@ -0,0 +1,44 @@
+#include <curl/curl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t got_data(char *buffer, size_t itemsize, size_t nitems, void* ignorethis){
+    size_t bytes = itemsize * nitems;
+    int linenumber = 1;
+    printf("How chunk (%zu bytes)\n", bytes);
+    printf("%d\t", linenumber);
+    for(int i = 0; i < bytes; i++){
+        printf("%c", buffer[i]);
+        if(buffer[i] == '\n'){
+            printf("\n");
+            linenumber++;
+             printf("%d:\t", linenumber);
+        }
+    }
+    printf("\n\n");
+    return bytes;
+}
+
+int main() {
+CURL *curl = curl_easy_init();
+
+if(!curl) {
+    fprintf(stderr, "init failed\n");
+}
+
+//set option.
+curl_easy_setopt(curl, CURLOPT_URL, "https://flutter.dev/");
+curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, got_data);
+
+
+
+//perform out action
+CURLcode result = curl_easy_perform(curl);
+if(result != CURLE_OK){
+    fprintf(stderr, "download problem: %s\n", curl_easy_strerror(result));
+}
+
+curl_easy_cleanup(curl);
+
+    return EXIT_SUCCESS;
+}
